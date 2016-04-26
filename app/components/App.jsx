ffcb1d6b9999af1f3b74329d1d6239a3629d6349
Sam Gruse <sgruse89@gmail.com>
2016-04-25 17:45:06 -0700

    CRUD is fully working and now doing styling

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,6 @@
 import uuid from 'node-uuid';
 import React from 'react';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
 
@@ -25,20 +26,22 @@ export default class App extends React.Component {
   }
 
 
-
-
-
-
   render() {
     const notes = this.state.notes
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
-          )}</ul>
+        <Notes notes={notes}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
       </div>
     );
+  }
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    })
   }
   addNote = () => {
     this.setState({
@@ -47,5 +50,17 @@ export default class App extends React.Component {
         task: 'New Task'
       }])
     })
+  }
+  editNote = (id, task) => {
+    if(!task.trim()) {
+      return;
+    }
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
   }
 }
